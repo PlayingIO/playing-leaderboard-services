af18f5093c9962d727dd103ca0611e7a7361685d
forafish <fora.luo@gmail.com>
2018-02-11 01:30:59 +0800

    use primitive type name

--- src/models/leaderboard-model.js
+++ src/models/leaderboard-model.js
@@ -6,40 +6,40 @@ import { models as rules } from 'playing-rule-services';
  * Leaderboard are a system which rank players based upon their scores.
  */
 const fields = {
-  name: { type: 'String', required: true },  // name for the leaderboard
-  description: { type: 'String' },           // brief description of the leaderboard
-  metric: {                                  // metric on which this leaderboard is based upon
+  name: { type: String, required: true },  // name for the leaderboard
+  description: { type: String },           // brief description of the leaderboard
+  metric: {                                // metric on which this leaderboard is based upon
     id: { type: 'ObjectId' },
-    type: { type: 'String', enum: [
+    type: { type: String, enum: [
       'point',
       'compound'
     ]}
   },
-  metric: {                                  // scope on which this leaderboard operates
-    id: { type: 'String' },                  // id of team definition or team instance
-    type: { type: 'String', enum: [
+  metric: {                                // scope on which this leaderboard operates
+    id: { type: String },                  // id of team definition or team instance
+    type: { type: String, enum: [
       'game',
       'team_definition',
       'team_instance',
       'custom'
     ]}
   },
-  type: { type: 'String', enum: [            // ranking type of the leaderboard
+  type: { type: String, enum: [            // ranking type of the leaderboard
     'regular']
   },
-  entityType: { type: 'String', enum: [      // type of leaderboard whether its for players or teams
+  entityType: { type: String, enum: [      // type of leaderboard whether its for players or teams
     'players',
     'teams'
   ]},
-  cycles: { type: 'String', enum: [          // interval in which the leaderboard is reset
+  cycles: { type: String, enum: [          // interval in which the leaderboard is reset
     'alltime',
     'yearly',
     'monthly',
     'weekly',
     'daily'
   ]},
-  requires: rules.rule.requires,             // visibility requirements for the leaderboard
-  tags: [{ type: 'String' }],                // tags of the leaderboard
+  requires: rules.rule.requires,           // visibility requirements for the leaderboard
+  tags: [{ type: String }],                // tags of the leaderboard
 };
 
 export default function model (app, name) {
